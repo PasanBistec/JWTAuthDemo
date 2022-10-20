634e5664ae2c6ab04e0f58cbf9d4a6d5109fe817
PasanBistec <pasan@bistecglobal.com>
2022-10-20 09:15:36 +0530

    Add project files.

---
+++ JWTAuthDemo/JwtAuthenticationManager.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
+
+namespace JWTAuthDemo
+{
+    public class JwtAuthenticationManager
+    {
+
+        private readonly string key;
+
+        private readonly IDictionary<string, string> users = new Dictionary<string, string>()
+        { {"test","password"},{"test1","pwd"}};
+
+        public JwtAuthenticationManager(string key)
+        {
+            this.key = key;
+        }
+
+        public string Authenticate(string username,string password)
+        {
+            if(!users.Any(u=> u.Key == username && u.Value == password))
+            { return null; }
+
+            JwtSecurityTokenHandler tokenHandler = new JwtSecurityTokenHandler();    
+
+            var tokenKey = Encoding.ASCII.GetBytes(key);
+
+
+
+            return null;
+        }
+    }
+}
